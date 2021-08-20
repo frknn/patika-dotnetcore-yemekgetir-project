51c9cc978a428340ba22ad3ec8f861da04501756
frknn <frknstbs@gmail.com>
2021-08-20 18:27:35 +0300

    Implement Update Order Status Command Validator

---
+++ YemekGetir/Application/OrderOperations/UpdateOrder/UpdateOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+using FluentValidation;
+using YemekGetir.Common;
+
+namespace YemekGetir.Application.OrderOperations.Commands.UpdateOrder
+{
+  public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
+  {
+    public UpdateOrderCommandValidator()
+    {
+      RuleFor(command => int.Parse(command.Id)).GreaterThan(0);
+      RuleFor(command => command.Model.StatusId).InclusiveBetween(2,3);
+    }
+  }
+
+}
